f4016545cc0c3c85c5f44e3a340923e0e9ba9bfc
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2015-08-05 15:13:41 +0200

    radar

--- app/scripts/directive/paperDirective.js
+++ app/scripts/directive/paperDirective.js
@@ -14,52 +14,54 @@ angular.module('testProjectApp')
         'boxWidth': 1800,
         //'boxHeight': window.innerHeight,
         'boxHeight': 1800,
-        'radarPositions': '',
-        'tooltip': ''
+        'tooltip': '',
+        'top': 250,
+        'left': 300,
+        'nodeActive': false,
+        'radar_arcs' : ['Hold', 'Assess', 'Trial', 'Adopt'],
+        'new_positions': []
       }
 
       var radius = directiveConf['boxWidth']/3;
+      var medRadius = radius/2;
+      var Nniveles = directiveConf.radar_arcs.length;
 
       radarData.then(function(data){
-        directiveConf['radarPositions'] = data;
-        //console.log(data);
-        //  _.each(data.nodes, function(value){
-        //    createNodes(value);
-        //  })
+        console.log(data);
+          _.each(data, function(value){
+            _.each(value.items, function(val){
+              createNodes(val,value.left,value.top, value.color, value.colorover);
+            })
+          })
         });
+
+      document.getElementById("chart").style.height = window.innerHeight + "px";
+      document.getElementById("chart").style.width = window.innerWidth + "px";
+      document.getElementById("chart").style.backgroundColor = "beige";
 
       var box = d3.select('#chart')
         .append('svg')
         .attr('class', 'box')
         .attr('width', radius)
         .attr('height', radius)
-        .append('g')
-        .attr('transform', 'translate(0,0)');
+
+      directiveConf['tooltip'] = box.append('text')
+        .style('opacity', 1)
+        .style('font-family', 'sans-serif')
+        .style('font-size', '13px');
 
 
-      var circle = box.append('circle')
-        .data(directiveConf['radarPositions'])
-        .enter().append('circle')
-        .attr('class', 'node')
-        .attr('r', 10)
-        .attr('class', 'draggableCircle')
-        .attr('cx', function (d) {
-          console.log(d)
-          return d.pc.r;
-        })
-        .attr('cy', function (d) {
-          return d.pc.t;
-        })
-        .attr('nombre', function (d) {
-          return d.color;
-        })
-        .style('fill', d3.rgb(72, 101, 145));
 
-      for ( var a= 0; a<5; a++){
-        drawRadarPanel(radius, a);
-      }
+      _.each(directiveConf.radar_arcs, function(d,i){
+        drawRadarPanel(d, i, Nniveles );
+      })
 
-   function createNodes(datos){
+      //for ( var a= 0; a<5; a++){
+      //  drawRadarPanel(radius, a);
+      //}
+
+   function createNodes(datos, top, left, color, colorOver){
+        console.log(datos, top, left, color, colorOver);
         var drag = d3.behavior.drag()
           .on('dragstart', function() {
             circle.style('fill', d3.rgb(41, 58, 82));
@@ -71,27 +73,42 @@ angular.module('testProjectApp')
             console.log(circle[0][0].attributes.cx.value+' - '+circle[0][0].attributes.cy.value);
             console.log(circle);
           })
-          .on('drag', function() { circle.attr('cx', d3.event.x)
+          .on('drag', function() {
+            circle.attr('cx', d3.event.x)
             .attr('cy', d3.event.y); })
           .on('dragend', function() {
+            console.log(circle[0][0].attributes);
             circle.style('fill', d3.rgb(191, 126, 48));
+            directiveConf['nodeActive'] = true;
             console.log(circle[0][0].attributes.cx.value+' - '+circle[0][0].attributes.cy.value );
+            directiveConf['new_positions']
+              .push({
+                "name": circle[0][0].attributes.nombre.nodeValue,
+                "pc": {
+                  "r": circle[0][0].attributes.cx.value,
+                  "t": circle[0][0].attributes.cy.value },
+                "movement": "c" })
+            console.log(directiveConf['new_positions']);
           });
 
         var circle = box.append('svg:circle')
           .attr('class', 'draggableCircle')
-          .attr('cx', datos.pc.r+datos.top)
-          .attr('cy', datos.pc.t+datos.left)
-          .attr('r', 5)
+          //.attr('cx', datos.pc.r+(directiveConf.left - left))
+          .attr('cx', datos.pc.r)
+          //.attr('cy', datos.pc.t+(directiveConf.top/2 + top))
+          .attr('cy', datos.pc.t)
+          .attr('r', 8)
           .attr('nombre', datos.name)
+          .attr('color', color)
+          .attr('colorover', colorOver)
           //.attr('tipo', datos.tipo)
-          .text(  datos.name )
+          .text( datos.name )
           .call(drag)
-          .style('fill', d3.rgb(72, 101, 145));
+          .style('fill', color);
 
         box.selectAll(".draggableCircle")
-          .on("mouseover", function(d, i) {
-            console.log(parseInt($(this).attr('cx'))+15);
+          .on("mouseover", function() {
+            directiveConf['nodeActive']=false;
             directiveConf['tooltip']
               .attr('x', parseInt($(this).attr('cx'))+15)
               .attr('y', $(this).attr('cy'))
@@ -101,46 +118,42 @@ angular.module('testProjectApp')
               .style('opacity', 1);
 
             d3.select(this)
-              .style('fill',d3.rgb(41, 58, 82));
+              .style('fill',$(this).attr('colorover'));
           })
           .on("mouseout", function(d, i) {
-            directiveConf['tooltip']
-              .transition(200)
-              .style('opacity', 0);
-            d3.select(this)
-              .style("fill",d3.rgb(72, 101, 145));
+            if(!directiveConf['nodeActive']){
+              directiveConf['tooltip']
+                .transition(200)
+                .style('opacity', 0);
+              d3.select(this)
+                .style("fill",$(this).attr('color'));
+            }
           })
       }
+      //setInterval(function(){
+      //  box.selectAll(".draggableCircle")
+      //    .transition()
+      //    .duration(2000)
+      //    .attr("cx", function(d,i){ return Math.floor((Math.random() * 600) + 10); })
+      //    //.attr("cx", function(d,i){ return +d3.selectAll(".draggableCircle")[0][i].cy.animVal.value - 5 })
+      //    .attr("cy", function(d,i){ return Math.floor((Math.random() * 600) + 10); })
+      //    //.attr("cy", function(d,i){ return +d3.selectAll(".draggableCircle")[0][i].cy.animVal.value - 5 });
+      //}, 5000);
 
-      setInterval(function(){
-        console.log(box.selectAll(".draggableCircle"));
-       box.selectAll(".draggableCircle")
-          .transition()
-          .duration(2000)
-          .attr("cx",function(d,i){
-           console.log(d,i);
-           return d.cx.value
-         })
-          .attr("cy",function(d,i){
-           console.log(d,i);
-           return d.cy.value
-         });
-      }, 5000);
-
-   function drawRadarPanel(radio, long){
+   function drawRadarPanel(cuadrantes, nivel, Nniveles){
         box.append('svg:circle').attr({
-          cx: radio/2,
-          cy: radio/2,
-          r: (radio/8)*long,
+          cx: radius/2,
+          cy: radius/2,
+          r: (medRadius/Nniveles)*nivel,
           class: 'serctorCircles',
           fill: "transparent",
           stroke: d3.rgb(90, 90, 90)
         });
        box.append('text')
-         .attr('x', 300)
-         .attr('y', (radio/8)*long)
+         .attr('x', medRadius)
+         .attr('y', ((medRadius/Nniveles)*nivel)+10)
          .attr("font-weight", "bold")
-         .text('trial')
+         .text( cuadrantes )
          .style('opacity', 1)
          .style('font-family', 'sans-serif')
          .style('font-size', '13px');
@@ -168,11 +181,6 @@ angular.module('testProjectApp')
           .attr('r', 5)
           .style('fill', d3.rgb(76, 76, 76));
       }
-
-      directiveConf['tooltip'] = box.append('text')
-        .style('opacity', 1)
-        .style('font-family', 'sans-serif')
-        .style('font-size', '13px');
     }
   }
 })
