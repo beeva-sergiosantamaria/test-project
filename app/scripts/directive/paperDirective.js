21108cdb981d01903d47a4e2935ee97870087ff9
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2015-08-07 13:49:24 +0200

    funcionalidades radar

--- app/scripts/directive/paperDirective.js
+++ app/scripts/directive/paperDirective.js
@@ -17,15 +17,39 @@ angular.module('testProjectApp')
         'left': 300,
         'nodeActive': false,
         'radar_arcs' : ['Hold', 'Assess', 'Trial', 'Adopt'],
-        'new_positions': []
+        'new_positions': [],
+        'nodesActives': []
       }
-      var activateNodeAnimation = true;
+      var nodesActives = true;
       var radius = directiveConf['boxWidth']/3;
       var medRadius = radius/2;
       var Nniveles = directiveConf.radar_arcs.length;
       var calculateMedia = (radius/2)/300;
-      var doble = 12*calculateMedia;
+      var doble = 16*calculateMedia;
       var normal = 6*calculateMedia;
+      var control = false;
+
+      document.getElementById("chart").style.height = window.innerHeight + "px";
+      document.getElementById("chart").style.width = window.innerWidth + "px";
+      document.getElementById("chart").style.backgroundColor = 'rgb(40, 44, 52)';
+
+      $(document).on('keyup keydown', function(e) {
+        control = e.ctrlKey;
+      });
+
+      $('#playButton').click(function(){
+        transitionNodes();
+      })
+
+      $('#resetButton').click(function(){
+        resetNodes();
+      })
+
+      var box = d3.select('#chart')
+        .append('svg')
+        .attr('class', 'box')
+        .attr('width', radius)
+        .attr('height', radius);
 
       radarData.then(function(data){
           _.each(data, function(value,a){
@@ -38,32 +62,9 @@ angular.module('testProjectApp')
           })
         });
 
-      document.getElementById("chart").style.height = window.innerHeight + "px";
-      document.getElementById("chart").style.width = window.innerWidth + "px";
-      document.getElementById("chart").style.backgroundColor = 'rgb(40, 44, 52)';
-
-      var box = d3.select('#chart')
-        .append('svg')
-        .attr('class', 'box')
-        .attr('width', radius)
-        .attr('height', radius)
-
-      directiveConf['tooltip'] = box.append('text')
-        .style('opacity', 1)
-        .style('font-family', 'sans-serif')
-        .style('font-size', '13px');
-
       _.each(directiveConf.radar_arcs, function(d,i){
         drawRadarPanel(d, i, Nniveles );
       })
-
-    function PlayButton(){
-      var button = document.createElement("div");
-      button.className = 'playButton';
-      button.textContent = '>'
-      button.id= 'playButton';
-      $('#chart').append(button);
-    }
 
    function CreateTableInf(cuadrante,valor){
      var clusterTitle = cuadrante.split(' ')[0];
@@ -107,24 +108,47 @@ angular.module('testProjectApp')
      $('.nodeElement')
        .on("mouseover", function() {
          var coloreDark = d3.select('#'+$(this).attr('id'))[0][0].attributes.colorover.value;
-         var radioAmp = d3.select('#'+$(this).attr('id'))[0][0].attributes.r.value;
-           d3.select('#'+$(this).attr('id'))
+         d3.select('#' + $(this).attr('id'))
+           .transition()
+           .attr('r', doble);
+         if(nodesActives) {
+           d3.select('#' + $(this).attr('id'))
              .transition()
-             .style("fill",coloreDark)
-             .attr('r', doble);
-           activateNodeAnimation = false;
-
+             .attr('r', doble)
+             .style("fill", coloreDark);
+            }
          })
-       .on("mouseout", function(d, i) {
-         console.log(d3.select('#'+$(this).attr('id')));
-         var radioAmp = d3.select('#'+$(this).attr('id'))[0][0].attributes.r.value;
+       .on("mouseout", function() {
          var colore = d3.select('#'+$(this).attr('id'))[0][0].attributes.color.value;
+         d3.select('#'+$(this).attr('id'))
+           .transition()
+           .attr('r', normal);
+         if(nodesActives){
            d3.select('#'+$(this).attr('id'))
              .transition()
-             .style("fill",colore)
-             .attr('r', normal);
-           activateNodeAnimation = true;
-
+             .attr('r', normal)
+             .style("fill",colore);
+          }
+       })
+       .on("mousedown", function(){
+         if(!control)directiveConf['nodesActives'] = [$(this).attr('id')];
+         if(control)directiveConf['nodesActives'].push($(this).attr('id'));
+         var nodos = box.selectAll(".draggableCircle")[0];
+         _.each(nodos, function(d){
+           if ($.inArray($(d).attr('id'), directiveConf['nodesActives']) == -1) {
+             d3.select(d)
+               .transition()
+               .duration(500)
+               .style("fill", 'white');
+             nodesActives = false;
+           } else {
+             d3.select(d)
+               .transition()
+               .duration(500)
+               .style("fill", $(d).attr('color'));
+             nodesActives = false;
+           }
+         })
        })
    }
 
@@ -143,18 +167,23 @@ angular.module('testProjectApp')
           })
           .on('drag', function() {
             circle.attr('cx', d3.event.x)
-            .attr('cy', d3.event.y); })
+              .attr('cy', d3.event.y); })
           .on('dragend', function() {
-            circle.style('fill', d3.rgb(200, 10, 10));
-            directiveConf['nodeActive'] = true;
-            directiveConf['new_positions']
-              .push({
-                "name": circle[0][0].attributes.nombre.nodeValue,
-                "pc": {
-                  "r": circle[0][0].attributes.cx.value,
-                  "t": circle[0][0].attributes.cy.value },
-                "movement": "c" })
-            console.log(directiveConf['new_positions']);
+            var coeficienteX = (parseInt(circle[0][0].attributes.cx.value) - parseInt(circle[0][0].attributes.originalcx.value));
+            var coeficienteY = (parseInt(circle[0][0].attributes.cy.value) - parseInt(circle[0][0].attributes.originalcy.value));
+            if( Math.abs(coeficienteX)>6 || Math.abs(coeficienteY)>6 ){
+              console.log( Math.abs(coeficienteX), Math.abs(coeficienteY) );
+              circle.style('fill', d3.rgb(200, 10, 10));
+              directiveConf['nodeActive'] = true;
+              directiveConf['new_positions']
+                .push({
+                  "name": circle[0][0].attributes.nombre.nodeValue,
+                  "pc": {
+                    "r": circle[0][0].attributes.cx.value,
+                    "t": circle[0][0].attributes.cy.value },
+                  "movement": "c" })
+              console.log(directiveConf['new_positions']);
+            }
           });
 
         var circle = box.append('svg:circle')
@@ -165,13 +194,20 @@ angular.module('testProjectApp')
           .attr('nombre', datos.name)
           .attr('color', color)
           .attr('colorover', colorOver)
+          .attr('originalcx', datos.pc.r*calculateMedia)
+          .attr('originalcy', datos.pc.t*calculateMedia)
           .attr('id', nombreTrim)
           .text( datos.name )
           .call(drag)
-          .style('fill', color);
+          .style('fill', color)
+          .style('stroke', color);
 
         box.selectAll(".draggableCircle")
           .on("mouseover", function() {
+            if(nodesActives){
+              d3.select(this)
+                .style('fill',$(this).attr('colorover'));
+            }
             $('#'+$(this).attr('id')+'.nodeElement')
               .removeClass('nodeElement')
               .addClass('nodeElementOver');
@@ -183,35 +219,74 @@ angular.module('testProjectApp')
               .text($(this).attr('nombre'))
               .transition(200)
               .style('opacity', 1);
-
-            d3.select(this)
-              .style('fill',$(this).attr('colorover'));
           })
-          .on("mouseout", function(d, i) {
-            $('#'+$(this).attr('id')+'.nodeElementOver')
-              .removeClass('nodeElementOver')
-              .addClass('nodeElement');
+          .on("mouseout", function() {
+            if ($.inArray($(this).attr('id'), directiveConf['nodesActives']) == -1) {
+              $('#' + $(this).attr('id') + '.nodeElementOver')
+                .removeClass('nodeElementOver')
+                .addClass('nodeElement');
+            }
             if(!directiveConf['nodeActive']){
               directiveConf['tooltip']
                 .transition(200)
                 .style('opacity', 0);
-              d3.select(this)
-                .style("fill",$(this).attr('color'));
+              if(nodesActives){
+                d3.select(this)
+                  .style("fill",$(this).attr('color'));
+              }
             }
           })
-          .on("click", function(d,i){
-            console.log(this);
+          .on("mousedown", function(){
+            if(!control)directiveConf['nodesActives'] = [$(this).attr('id')];
+            if(control)directiveConf['nodesActives'].push($(this).attr('id'));
+            var nodos = box.selectAll(".draggableCircle")[0];
+            _.each(nodos, function(d){
+              if ($.inArray($(d).attr('id'), directiveConf['nodesActives']) == -1) {
+                $('#'+$(d).attr('id')+'.nodeElementOver')
+                  .removeClass('nodeElementOver')
+                  .addClass('nodeElement');
+                    d3.select(d)
+                      .transition()
+                      .duration(500)
+                      .style("fill", 'white');
+                    nodesActives = false;
+                } else {
+                $('#'+$(d).attr('id')+'.nodeElement')
+                  .removeClass('nodeElement')
+                  .addClass('nodeElementOver');
+                d3.select(d)
+                  .transition()
+                  .duration(500)
+                  .style("fill", $(d).attr('color'));
+                nodesActives = false;
+              }
+            })
           })
       }
-    function transitionNodes(){
-       console.log('entra');
-        box.selectAll(".draggableCircle")
-          .transition()
-          .duration(2000)
-          .attr("cx", function(d,i){ return Math.floor((Math.random() * 600) + 10); })
-          //.attr("cx", function(d,i){ return +d3.selectAll(".draggableCircle")[0][i].cy.animVal.value - 5 })
-          .attr("cy", function(d,i){ return Math.floor((Math.random() * 600) + 10); })
-          //.attr("cy", function(d,i){ return +d3.selectAll(".draggableCircle")[0][i].cy.animVal.value - 5 });
+
+      function transitionNodes(){
+        var nodos = box.selectAll(".draggableCircle")[0];
+        _.each(nodos, function(d){
+          if(directiveConf['nodesActives'].length>0){
+            if ($.inArray($(d).attr('id'), directiveConf['nodesActives']) != -1) {
+              d3.select(d)
+                .transition()
+                .duration(2000)
+                .attr("cx",  parseInt($(d).attr('cx')) + (Math.floor((Math.random() * 50) - 20)) )
+                .attr("cy",  parseInt($(d).attr('cy')) + (Math.floor((Math.random() * 50) - 20)) );
+            }
+          }
+          else{
+            d3.select(d)
+              .transition()
+              .duration(2000)
+              .attr("cx",  parseInt($(d).attr('cx')) + (Math.floor((Math.random() * 30) - 20)) )
+              .attr("cy",  parseInt($(d).attr('cy')) + (Math.floor((Math.random() * 30) - 20)) );
+          }
+          //console.log(d);
+          //console.log($(d).attr('cx'));
+
+        })
       };
 
    function drawRadarPanel(cuadrantes, nivel, Nniveles){
@@ -252,9 +327,50 @@ angular.module('testProjectApp')
           .attr('class', 'circleCenterFixed')
           .attr('cx', radius/2)
           .attr('cy', radius/2)
-          .attr('r', 5)
-          .style('fill', d3.rgb(76, 76, 76));
+          .attr('r', 7)
+          .style('cursor', 'pointer')
+          .style('fill', d3.rgb(76, 76, 76))
+          .on("mouseover", function() {
+            directiveConf['tooltip']
+              .attr('x', (radius/2)+10)
+              .attr('y', (radius/2)-10)
+              .attr("font-weight", "bold")
+              .text('Pulsar para restaurar')
+              .transition(200)
+              .style('opacity', 1);
+          })
+          .on("mousedown", function() {
+            resetNodes();
+            });
+
+         setInterval(function(){
+           if(circleCenter.attr('r')==7) var crescentRadio = 12;
+           else var crescentRadio = 7;
+           circleCenter
+             .transition()
+             .attr('r', crescentRadio);
+         }, 600);
       }
+      function resetNodes(){
+        directiveConf['nodesActives'] = [];
+        nodesActives = true;
+        var nodos = box.selectAll(".draggableCircle")[0];
+        _.each(nodos, function(d){
+          $('#'+$(d).attr('id')+'.nodeElementOver')
+            .removeClass('nodeElementOver')
+            .addClass('nodeElement');
+          d3.select(d)
+            .transition()
+            .duration(500)
+            .style("fill", $(d).attr('color'));
+        })
+      }
+
+      directiveConf['tooltip'] = box.append('text')
+        .style('opacity', 1)
+        .style('font-family', 'sans-serif')
+        .style('font-size', '13px');
+
     }
   }
 })
